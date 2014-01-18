7a18ccf9d5246bc2bba723e2457f8555dfe64c26
Shareef Ali <shareefalis@cyanogenmod.org>
2014-01-18 04:18:28 -0600

    hlte: make tree universal.
    * do what hammerhead did.
    * shove cdma and gsm into one universal tree based on hardware
    revision.
    * this is for bcm nci
    
    Change-Id: I4f2063ad5296297a945976ca011823c2a0487129

--- cm.mk
+++ cm.mk
@@ -1,4 +1,4 @@
-$(call inherit-product, device/samsung/hltexx/full_hltexx.mk)
+$(call inherit-product, device/samsung/hltenci/full_hltenci.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/gsm.mk)
@@ -12,6 +12,6 @@ $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=hltexx TARGET_DEVICE=hltexx BUILD_FINGERPRINT="samsung/hltetmo/hltetmo:4.3/JSS15J/N900TUVUBMI7:user/release-keys" PRIVATE_BUILD_DESC="hltetmo-user 4.3 JSS15J N900TUVUBMI7 release-keys"
 
-PRODUCT_DEVICE := hltexx
-PRODUCT_NAME := cm_hltexx
+PRODUCT_DEVICE := hltenci
+PRODUCT_NAME := cm_hltenci
 
